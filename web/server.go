8592d4ab2895b6fbed6b48403afad93cd6db4862
Louis Billiet <billiet.l@sfeir.com>
2021-10-26 17:59:59 +0200

    init logger

--- web/server.go
+++ web/server.go
@@ -2,11 +2,19 @@ package web
 
 import (
 	"fmt"
-	"log"
 	"net/http"
+	"os"
 	"price-comparator/dao"
 	"price-comparator/web/api"
 	"price-comparator/web/pages"
+	"time"
+
+	"github.com/gorilla/handlers"
+	"github.com/justinas/alice"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/diode"
+	"github.com/rs/zerolog/hlog"
+	"github.com/rs/zerolog/log"
 )
 
 type Server struct {
@@ -43,16 +51,44 @@ func MakeServer(port int, dao *dao.Bundle) (server *Server) {
 	}
 }
 
-func (serv Server) Run() error {
+func (serv Server) computeMiddlewares(appRouter *Router) (finalRouter http.Handler) {
+	chain := alice.New()
+	// Logging middleware
+	wr := diode.NewWriter(os.Stdout, 1000, 10*time.Millisecond, func(missed int) {
+		fmt.Printf("Logger Dropped %d messages", missed)
+	})
+	logger := zerolog.New(wr).With().
+		Timestamp().
+		Logger()
+	chain = chain.Append(hlog.NewHandler(logger))
+	chain = chain.Append(hlog.AccessHandler(func(r *http.Request, status, size int, duration time.Duration) {
+		hlog.FromRequest(r).Info().
+			Str("method", r.Method).
+			Stringer("url", r.URL).
+			Int("status", status).
+			Int("size", size).
+			Dur("duration", duration).
+			Msg("")
+	}))
+	chain = chain.Append(hlog.RemoteAddrHandler("ip"))
+	chain = chain.Append(hlog.UserAgentHandler("user_agent"))
+	chain = chain.Append(hlog.RefererHandler("referer"))
+	chain = chain.Append(hlog.RequestIDHandler("req_id", "Request-Id"))
+	// Performance middleware
+	chain = chain.Append(handlers.CompressHandler)
+	return chain.Then(serv.router)
+}
 
-	log.Print(fmt.Sprintf("Starting server on port %d", serv.port))
-	log.Fatal(
+func (serv Server) Run() error {
+	log.Info().Msg(fmt.Sprintf("Starting server on port %d", serv.port))
+	log.Fatal().Msgf(
+		"%s",
 		http.ListenAndServe(
 			fmt.Sprintf(":%d", serv.port),
-			serv.router,
+			serv.computeMiddlewares(serv.router),
 		),
 	)
-	log.Print("Shuting server")
+	log.Info().Msg("Shuting server")
 
 	return nil
 }
